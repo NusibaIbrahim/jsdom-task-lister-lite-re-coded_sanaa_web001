50616dc65956f89a5731c31fb641a8ed3a79fe81
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-17 21:24:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
   e.preventDefault();
-  let input = document.getElementById("new-task-description")
+  let input = document.getElementById("new-task-description");
+  let btn = document.getElementById
+  let onclick = Function 
 });
