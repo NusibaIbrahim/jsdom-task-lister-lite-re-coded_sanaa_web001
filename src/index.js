4c15eacdcebc4f1b4ffd9a44cf0ed36553b90870
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-17 21:30:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
   let input = document.getElementById("new-task-description");
   let btn = document.getSelector("input[type=submit]");
-  let list = document.getElementById("list");
+  let list = document.getElementById("#list #tasks");
   btn.onclick = function(){
     
   };
