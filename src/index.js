86f746792e3be413f21af1c8c9d53efa7074a0bd
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-17 21:51:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const taskList = document.querySelector("#tasks");
 taskForm.addEventListener("DOMContentLoaded", (e) => {
   // your code here
   e.preventDefault();
-//   let newTask = document.getElementById("new-task-description");
+let newTask = document.getElementById("new-task-description").value;
 //   let btn = document.querySelector("input[type=submit]");
 //   let list =
 //   btn.onclick = function(){
