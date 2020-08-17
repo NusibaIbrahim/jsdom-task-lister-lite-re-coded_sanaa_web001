7becfd9111cdb2aac22030bd061bc502e4ff14b0
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-17 21:50:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const main = document.querySelector("#main-content");
 const taskForm = document.querySelector("#create-task-form");
+const taskList = document.querySelector("#tasks");
 
 document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
   e.preventDefault();
 //   let input = document.getElementById("new-task-description");
 //   let btn = document.querySelector("input[type=submit]");
-//   let list = document.querySelector("#list #tasks");
+//   let list =
 //   btn.onclick = function(){
 //     list.innerHTML = input.value;
 //   };
